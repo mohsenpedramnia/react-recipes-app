a390a811089ac5288d0594c7feb1e2c32a3eff2b
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-10-31 16:03:49 +0330

    Adding new title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return <div>hello from our recipes app</div>;
+  return (
+    <div>
+      <h1>VipRecipes</h1>
+      <p>Celebrating with this iframe tag</p>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/xy96ui8kRYg"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 }
 
 export default App;
