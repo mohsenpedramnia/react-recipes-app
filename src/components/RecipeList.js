f4d351732140695d9c022aa373771edf6557136b
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-11-07 23:20:18 +0330

    Recipe list component structure

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -2,11 +2,25 @@ import React, { Component } from "react";
 import Recipe from "./Recipe";
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
-      <div>
-        hello from recipe list
-        <Recipe />
-      </div>
+      <>
+        <div className="container py-5">
+          {/* title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/* end of title */}
+          <div className="row">
+            {recipes.map(recipe => (
+              <Recipe key={recipe.recipe_id} recipe={recipe}></Recipe>
+            ))}
+          </div>
+          <Recipe />
+        </div>
+      </>
     );
   }
 }
